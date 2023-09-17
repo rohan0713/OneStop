85e33cc7676858b8fe2f452e9024509ac7c54084
RohanRamola <rohanramola63@gmail.com>
2023-09-17 18:05:18 +0530

    flatmap operator

--- app/src/main/java/com/kotlin/androidtopics/rxjava/ui/activities/RxjavaActivity.kt
+++ app/src/main/java/com/kotlin/androidtopics/rxjava/ui/activities/RxjavaActivity.kt
@@ -6,12 +6,15 @@ import android.util.Log
 import com.jakewharton.rxbinding4.view.clicks
 import com.kotlin.androidtopics.R
 import com.kotlin.androidtopics.databinding.ActivityRxjavaBinding
+import com.kotlin.androidtopics.rxjava.data.models.ApiResponse
 import com.kotlin.androidtopics.rxjava.data.remote.Retrofit
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.functions.Predicate
 import io.reactivex.rxjava3.schedulers.Schedulers
 import java.lang.IllegalArgumentException
 import java.util.concurrent.TimeUnit
@@ -30,15 +33,45 @@ class RxjavaActivity : AppCompatActivity() {
             .subscribe{
             networkCall()
         }
+
+        observer()
+    }
+
+    private fun observer() {
+        val observable = Observable.just("hello") // Created a Observable stream using just
+            .flatMap {
+                fetch().toObservable()  // flat map the data returned from fetch() into observable stream
+            }
+
+        observable.subscribe {
+            Log.d("subs", "$it")
+        }
     }
 
     private fun networkCall() {
         val service = Retrofit.api
-        service.getMeals("Indian").subscribeOn(Schedulers.io())
+        val observable = service.getMeals("Indian")
+            .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe{
-                Log.d("subs", it.meals.toString())
+
+        observable.subscribe(object : Observer<ApiResponse>{
+            override fun onSubscribe(d: Disposable) {
+                Log.d("subs", "On Subscribe is called")
             }
+
+            override fun onNext(t: ApiResponse) {
+                Log.d("subs", "On Next is called - ${t.meals}")
+            }
+
+            override fun onError(e: Throwable) {
+                Log.d("subs", "On Error is called")
+            }
+
+            override fun onComplete() {
+                Log.d("subs", "On Complete is called")
+            }
+        })
+
     }
 
     private fun createObservable() {
@@ -92,5 +125,11 @@ class RxjavaActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    private fun fetch() : Single<List<String>> {
+        return Single.fromCallable {
+            listOf("alpha", "beta")
+        }.subscribeOn(Schedulers.io())
     }
 }
